564e7703b593fb5b77384fdbb2e96914b7bb58ec
Miguel Galvan <60816395+mgalvang@users.noreply.github.com>
2025-10-09 18:22:13 +0000

    Correcting line 28

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,7 +25,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
     machine_type = "e2-medium"
 
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    service_account = "sa-github-actions@gke-project-prueba.iam.gserviceaccount.com"l
+    service_account = "sa-github-actions@gke-project-prueba.iam.gserviceaccount.com"
     oauth_scopes    = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
