0fc48a2f5105feaf94be7797f193860464a69311
Miguel Galvan <60816395+mgalvang@users.noreply.github.com>
2025-10-10 18:12:22 +0200

    Update credentials

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "google_container_cluster" "primary" {
   location = var.region
 
   remove_default_node_pool = true
-  initial_node_count       = 2
+  initial_node_count       = 1
 }
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
