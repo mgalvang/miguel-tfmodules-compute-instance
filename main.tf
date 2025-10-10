a15033d205226073130aca841ad2180ac86650b6
Miguel Galvan <60816395+mgalvang@users.noreply.github.com>
2025-10-10 17:58:13 +0200

    Change nodes

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "google_container_cluster" "primary" {
   location = var.region
 
   remove_default_node_pool = true
-  initial_node_count       = 1
+  initial_node_count       = 2
 }
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
